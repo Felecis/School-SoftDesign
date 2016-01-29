e867bad26912d3be2cec35bc679facfc1f6878f5
Felecis <Felecis@yandex.ru>
2016-01-30 02:35:19 +0400

    Revert "Добавлен логгер, реализован и залогирован жизненный цикл активити"

--- app/src/main/java/com/softdesign/school/MainActivity.java
+++ app/src/main/java/com/softdesign/school/MainActivity.java
@@ -2,71 +2,12 @@ package com.softdesign.school;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.CheckBox;
-import android.widget.Toast;
 
-import com.softdesign.school.utils.Lg;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    CheckBox mCheckBox;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Lg.e(this.getLocalClassName(), "on create");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mCheckBox = (CheckBox) findViewById(R.id.checkBox);
-        mCheckBox.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        int id = v.getId();
-        switch (id) {
-            case R.id.checkBox:
-                Toast.makeText(this, "Click!", Toast.LENGTH_SHORT).show();
-                break;
-
-        }
-
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Lg.e(this.getLocalClassName(), "on start");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Lg.e(this.getLocalClassName(), "on resume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Lg.e(this.getLocalClassName(), "on pause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Lg.e(this.getLocalClassName(), "on stop");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Lg.e(this.getLocalClassName(), "on restart");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Lg.e(this.getLocalClassName(), "on destroy");
     }
 }
